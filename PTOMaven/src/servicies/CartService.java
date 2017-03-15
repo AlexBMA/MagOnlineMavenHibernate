ce7007737180202303e84c25516899234f6645d8
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-03-15 21:30:22 +0200

    ..

--- PTOMaven/src/servicies/CartService.java
+++ PTOMaven/src/servicies/CartService.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import dboperations.DBOperations;
 import dboperations.DBOperationsCart;
 import modelMag.Cart;
+import modelMag.Product;
+import modelMag.ProductFromCart;
 
 public class CartService implements GeneralServiceInterface<Cart> {
 
@@ -46,6 +48,32 @@ public class CartService implements GeneralServiceInterface<Cart> {
 		DBOperations<Cart> cartOperations = new DBOperationsCart();
 		cartOperations.insert(session, item);
 		
+		GeneralServiceInterface<Product> productServices = new ProductService();
+		
+		List<ProductFromCart> list = item.getProductsFromCart();
+		updateStoks(session, productServices, list);
+		
 	}
+
+	public void updateStoks(SessionFactory session, GeneralServiceInterface<Product> productServices,
+		
+		List<ProductFromCart> list) {
+		int size = list.size();
+		ProductFromCart temp;
+		Product tempProduct;
+		int tempCantitateComadata;
+		
+		for( int  i=0;i<size;i++)
+		{ 	
+			temp = list.get(i);
+			tempCantitateComadata = temp.getCantitateComandata();
+			tempProduct = temp.getProdus();
+			tempProduct.setNumberOfItmes((tempProduct.getNumberOfItems()-tempCantitateComadata));
+			
+			productServices.updateItem(tempProduct, session);
+		}
+	}
+	
+	
 
 }
